79cd383d5fef3f1d8596fa5cce2e6839d92cb5ef
Pavan Chanduri <pavcha@amazon.com>
2025-04-24 00:05:56 +0530

    Added programs for MaxLenSubArrayWithSum0 and SubArraysWithGivenXOR

---
+++ src/ArrayProblems/MaxLengthSubArrayWithSum0.java
@@ -0,0 +1,44 @@
+package ArrayProblems;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class MaxLengthSubArrayWithSum0 {
+    public static void main(String[] args) {
+        int[] arr = {1, 0, -4, 3, 1, 0};
+        System.out.println(maxLen(arr));
+    }
+
+    private static int maxLen(int[] arr) {
+
+        int[] prefixSum = new int[arr.length];
+        prefixSum[0] = arr[0];
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        int maxLength = 0;
+        int length = 0;
+
+        for(int i=1;i<arr.length;i++) {
+            prefixSum[i] = prefixSum[i-1] + arr[i];
+        }
+
+        for(int i=0;i<prefixSum.length;i++) {
+            if(prefixSum[i]==0) {
+                length = i+1;
+            }
+        }
+
+        maxLength = Math.max(length, maxLength);
+
+        for(int i=0;i<prefixSum.length;i++) {
+            if(map.containsKey(prefixSum[i])) {
+                map.put(prefixSum[i], Arrays.asList(map.get(prefixSum[i]).getFirst(), i));
+            } else {
+                map.put(prefixSum[i], Arrays.asList(i,i));
+            }
+            length = map.get(prefixSum[i]).getLast()-map.get(prefixSum[i]).getFirst();
+            maxLength = Math.max(length, maxLength);
+        }
+        return maxLength;
+    }
+}
