515342b6054a9831f6bb0ca94967f27937c45379
Pavan Chanduri <pavcha@amazon.com>
2025-04-25 11:31:08 +0530

    Added NQueens, NMeetings and Palindrome Partitioning Problems

---
+++ src/ArrayProblems/NMeetings.java
@@ -0,0 +1,69 @@
+package ArrayProblems;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 1. Idea here is to sort the meeting list by its end time
+ * 2. Created a custom comparator to compare the end time and swap the meetings
+ * 3. If two meetings have the same end time, sort them as per their position
+ * 4. Compare the last meeting end time i.e., limit with the next meeting start time
+ *    and if next meeting start is more than previous meeting end time, store it in the result.
+ */
+public class NMeetings {
+
+    //Store meeting attributes as reference data type
+    public static class Meeting {
+        int start;
+        int end;
+        int position;
+        Meeting(int start, int end, int position) {
+            this.start = start;
+            this.end = end;
+            this.position = position;
+        }
+    }
+
+    static class MeetingComparator implements Comparator<Meeting> {
+        @Override
+        public int compare(Meeting o1, Meeting o2) {
+            if(o1.end > o2.end) {
+                return 1; //Swaps o1 and o2
+            } else if(o1.end < o2.end) {
+                return -1; //Does nothing
+            } else if(o1.position < o2.position) {
+                return -1; //if meeting end time is same, swap them as per position
+            }
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] start = {1,0,3,8,5,8};
+        int[] end = {2,6,4,9,7,9};
+
+        List<Meeting> meet = new ArrayList<>();
+
+        for(int i=0; i<start.length; i++) {
+            meet.add(new Meeting(start[i],end[i], i+1));
+        }
+
+        MeetingComparator mc = new MeetingComparator();
+        meet.sort(mc); //Sort the meetings as per their end time using Meeting Comparator
+        List<Integer> result = new ArrayList<>();
+        result.add(meet.getFirst().start);
+        int limit = meet.getFirst().end;
+        for(int i=1; i<start.length; i++) {
+            //compare last meeting endtime with next meeting start time
+            if(meet.get(i).start>limit) {
+                result.add(meet.get(i).position);
+                limit = meet.get(i).end; //update limit to the next meeting end time
+            }
+        }
+
+        for(int i=0; i<result.size(); i++) {
+            System.out.println(result.get(i));
+        }
+    }
+}
---
+++ src/Strings/PalindromePartitioning.java
@@ -0,0 +1,53 @@
+package Strings;
+
+import java.util.*;
+
+public class PalindromePartitioning {
+
+    public static List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        List<String> path = new ArrayList<>();
+        partitionHelper(s, path, res);
+        return res;
+    }
+
+    static void partitionHelper(String s, List<String> path, List<List<String>> res) {
+        if (s.isEmpty()) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i=0; i<s.length();++i) {
+            String part = s.substring(0, i+1);
+            if (isPalindrome(part)) {
+                path.add(part);
+                partitionHelper(s.substring(i+1), path, res);
+                path.remove(part);
+            }
+        }
+    }
+
+    static boolean isPalindrome(String s) {
+        int start = 0, end = s.length()-1;
+        while (start <= end) {
+            if (s.charAt(start++) != s.charAt(end--))
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "aabb";
+        List<List<String>> ans = partition(s);
+        int n = ans.size();
+        System.out.println("The Palindromic partitions are :-");
+        System.out.print(" [ ");
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print("[ ");
+            for (int j = 0; j < ans.get(i).size(); j++) {
+                System.out.print(ans.get(i).get(j) + " ");
+            }
+            System.out.print("] ");
+        }
+        System.out.print("]");
+    }
+}
