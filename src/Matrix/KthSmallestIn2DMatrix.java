459633edef5381004ca3c37e24929e27ec9a9a22
Pavan Chanduri <pavcha@amazon.com>
2025-04-25 23:27:00 +0530

    Added code for KthSmallestIn2DMatrix

--- src/Matrix/KthSmallestIn2DMatrix.java
+++ src/Matrix/KthSmallestIn2DMatrix.java
@@ -1,36 +1,33 @@
-package ArrayProblems;
+package Matrix;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
-/**
- Run a loop until k-1 and make the previous minElement the max element i.e., MAX_VALUE
- This way the next element would be the minimum element which would be returned
- */
-public class FindKSmallestElement {
+public class KthSmallestIn2DMatrix {
 
     public static void main(String[] args) {
-
-        int[] arr = {10,-2,-5, 1,5,7,9,-6,13};
-        int k = 1;
-        int minElement = Integer.MAX_VALUE;
-
-        // Find the k-th largest element
-        int kthSmallest = findKthSmallest(arr, k);
-        System.out.println(kthSmallest);
+        int[][] mat =     {{16, 28, 60, 64},
+                           {22, 41, 63, 91},
+                           {27, 50, 87, 93},
+                           {36, 78, 87, 94 }};
+        int k = 3;
+        System.out.println(kthSmallest(mat,mat.length,k));
     }
 
-    private static int findKthSmallest(int[] arr, int k) {
+    public static int kthSmallest(int[][] mat, int n, int k) {
+
         // Min-heap to store the k smallest elements through a custom comparator
         PriorityQueue<Integer> minHeap = new PriorityQueue<>(new CustomComparator());
 
         // Iterate through all elements
-        for (int num : arr) {
-            minHeap.add(num); // Add the current element to the heap
+        for(int i=0;i<mat.length;i++) {
+            for(int j=0;j<mat[i].length;j++) {
+                minHeap.add(mat[i][j]); // Add the current element to the heap
 
-            // If the heap size exceeds k, remove the smallest element
-            if (minHeap.size() > k) {
-                minHeap.poll();
+                // If the heap size exceeds k, remove the smallest element
+                if (minHeap.size() > k) {
+                    minHeap.poll();
+                }
             }
         }
 
@@ -42,7 +39,6 @@ public class FindKSmallestElement {
 
         @Override
         public int compare(Integer number1, Integer number2) {
-
             if(number1 < number2) {
                 return 1; //Swap
             } else if(number1 > number2) {
